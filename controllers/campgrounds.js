2c87e8bab2245833a34629a2e1be3fe44c0666f6
rkhaitu <raishkhaitu@gmail.com>
2021-02-21 18:14:11 +0000

    upload image option added

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -15,14 +15,12 @@ module.exports.createCampground = async (req, res, next) => {
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
-    console.log(campground);
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.showCampgrounds = async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate({ path: 'reviews', populate: { path: 'author' } }).populate('author');
-    console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
