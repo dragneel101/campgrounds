73568af12c51955bbe1038ccc3eb006bbba226c7
rkhaitu <raishkhaitu@gmail.com>
2021-02-21 18:09:16 +0000

    upload image option added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,15 +1,24 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const Campground = require('../models/campground');
 const campgrounds = require('../controllers/campgrounds')
+const { storage } = require('../cloudinary')
+const multer = require('multer')
+const upload = multer({ storage })
 
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+
+
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
--- seeds/index.js
+++ seeds/index.js
@@ -16,6 +16,31 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+
+
+const imagee = ["https://res.cloudinary.com/testserver/image/upload/v1613930125/YelpCamp/chris-schog-EnCaUE4QNOw-unsplash_shc1mb.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930124/YelpCamp/wei-pan-Ta0A1miYZKc-unsplash_oqjtur.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930124/YelpCamp/goutham-krishna-lITrCLM6hHc-unsplash_jk6oeg.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930124/YelpCamp/ken-cheung-TEriGq5ywYA-unsplash_tk1b07.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930123/YelpCamp/adore-chang-rRljZzjNQAA-unsplash_crnklb.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930123/YelpCamp/scott-goodwill-y8Ngwq34_Ak-unsplash_oyddvw.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930123/YelpCamp/234843649_pz1lyj.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/david-schultz-f5sdemaT7XE-unsplash_illzhp.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/olakira-camp-asilia-africa_uym91e.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/natura-eco-camp_ogghrr.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930123/YelpCamp/scott-goodwill-y8Ngwq34_Ak-unsplash_oyddvw.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930123/YelpCamp/234843649_pz1lyj.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/david-schultz-f5sdemaT7XE-unsplash_illzhp.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/olakira-camp-asilia-africa_uym91e.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/natura-eco-camp_ogghrr.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/campfire-muskoka-cottage_pselab.webp",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930122/YelpCamp/download_fvta8y.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930121/YelpCamp/download_1_dgnnks.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930121/YelpCamp/images_thmcb2.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930121/YelpCamp/images_1_b6zaad.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930121/YelpCamp/images_3_cr59fm.jpg",
+    "https://res.cloudinary.com/testserver/image/upload/v1613930121/YelpCamp/images_2_afwpt5.jpg"]
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
@@ -28,7 +53,15 @@ const seedDB = async () => {
             author: '60314175808171440e208375',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [{
+                url: imagee[Math.floor(Math.random() * imagee.length)],
+                filename: `YelpCamp/jerst6ffm1ef8p6uxvxv+${i}`
+            },
+            {
+                url: imagee[Math.floor(Math.random() * imagee.length)],
+                filename: `YelpCamp/jerst6ffm1ef8p6uxvxv+${i + i}`
+            }
+            ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
