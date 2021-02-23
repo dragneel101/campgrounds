d8c0369929a66e0ba75032060359514167fe6678
rkhaitu <raishkhaitu@gmail.com>
2021-02-23 21:15:32 +0000

    combined sessions

--- app.js
+++ app.js
@@ -50,30 +50,26 @@ app.use(mongoSanitize({
 }))
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
-const store = MongoStore.create({
-    mongoUrl: dbURL,
-    touchAfter: 24 * 3600
-});
 
-store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e)
-})
-
-const sessionConfig = {
+app.use(session({
     name: 'session',
     secret,
-    resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false, // don't create session until something stored
+    resave: false, //don't save session if unmodified
     cookie: {
         httpOnly: true,
         // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     },
-    store
-}
+    store: MongoStore.create({
+        mongoUrl: dbURL,
+        touchAfter: 24 * 3600, // time period in seconds
+        secret
 
-app.use(session(sessionConfig));
+    })
+
+}));
 app.use(flash());
 app.use(helmet());
 
